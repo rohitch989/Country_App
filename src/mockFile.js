8f98f8074332b916a173a873b4a73b86e318368c
Rohit Kumar <rohitch989@gmail.com>
2022-01-11 16:11:48 +0530

    v2

--- src/mockFile.js
+++ src/mockFile.js
@@ -6,6 +6,7 @@ export const continent = ["africa", "americas", "asia", "europe", "oceania"];
 export const fetchRegionCountriesName = async (region) => {
   let response = await Axios.get(`https://restcountries.com/v3.1/region/${region}`);
   let countrys = await response.data;
+  console.log(countrys)
   let countries = countrys.map(item => item.name.common);
   return countries;
 }
